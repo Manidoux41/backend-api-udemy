08785be15b1c0be1b3015cf52e30ddbbb666caf0
superflyman90 <superflyman90@gmail.com>
2022-12-18 01:47:02 +0100

    second send without .env

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -13,10 +13,30 @@ export const postTest = (req, res) => {
 
 export const addRoom = async (req,res) => {
     const room = new RoomModel(req.body)
-    try {
-        await room.save();
-        res.send(room)
-    } catch (error) {
-        res.status(500).send(error)
+    await room.save();
+    res.send(room)
+}
+
+export const getRooms = async (req,res) => {
+    const rooms = await RoomModel.find({})
+    res.send(rooms)
+}
+
+export const getRoom = async (req,res) => {
+    const room = await RoomModel.find({_id: req.params.id})
+    res.send(room)
+}
+
+export const updateRoom = async (req,res) => {
+    const room = await RoomModel.findByIdAndUpdate(req.params.id, req.body)
+    await room.save()
+    res.send(room)
+}
+
+export const deleteRoom = async (req,res) => {
+    const room = await RoomModel.findByIdAndDelete(req.params.id)
+    if (!room) {
+        res.status(404).send('Aucune chambre trouvée')
     }
+    res.status(200).send()    
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { addRoom, getTest, postTest } from "../controllers/roomControllers.js";
+import { catchErrors } from "../config/helpers.js";
+import { addRoom, deleteRoom, getRoom, getRooms, getTest, postTest, updateRoom } from "../controllers/roomControllers.js";
 
 const router = Router();
 
@@ -8,9 +9,15 @@ router.get('/', (_, res) => {
 })
 
 router.get('/test', getTest)
-
 router.post('/test', postTest)
 
-router.post('/room', addRoom);
+router.post('/room', catchErrors(addRoom));
+router.get('/rooms', catchErrors(getRooms));
+router.get('/room/:id', catchErrors(getRoom));
+router.patch('/room/:id', catchErrors(updateRoom));
+router.delete('/room/:id', catchErrors(deleteRoom));
+
+
+
 
 export default router;
