ebb410eb5f98324dc531b8cba60712d83a75f1cd
superflyman90 <superflyman90@gmail.com>
2022-12-18 06:56:26 +0100

    7

--- routes/routes.js
+++ routes/routes.js
@@ -2,20 +2,24 @@ import { Router } from "express";
 import { catchErrors } from "../config/helpers.js";
 import { addRoom, deleteRoom, getRoom, getRooms, getTest, postTest, updateRoom } from "../controllers/roomControllers.js";
 
+/* Path avec ES modules*/ 
+import path, { dirname } from 'path';
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const router = Router();
 
-router.get('/', (_, res) => {
-    res.send('Hello Everybody')
-})
 
-router.get('/test', getTest)
-router.post('/test', postTest)
+router.get('/api/rooms', catchErrors(getRooms));
 
-router.post('/room', catchErrors(addRoom));
-router.get('/rooms', catchErrors(getRooms));
-router.get('/room/:id', catchErrors(getRoom));
-router.patch('/room/:id', catchErrors(updateRoom));
-router.delete('/room/:id', catchErrors(deleteRoom));
+router.get('/api/rooms/:id', catchErrors(getRoom));
+
+router.post('/api/rooms', catchErrors(addRoom));
+
+router.patch('/api/rooms/:id', catchErrors(updateRoom));
+
+router.delete('/api/rooms/:id', catchErrors(deleteRoom));
 
 
 
